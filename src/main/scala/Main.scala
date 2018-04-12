ff8082b873df82086524256e434287e194c797f1
Juan Marin Otero <juan.marin.otero@gmail.com>
2018-04-12 12:35:30 -0400

    Make kafka servers configurable

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -8,29 +8,34 @@ import org.apache.kafka.clients.producer.ProducerRecord
 import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.common.serialization.{ByteArraySerializer, StringSerializer}
 import org.apache.kafka.common.serialization.{ByteArrayDeserializer, StringDeserializer}
+import com.typesafe.config.ConfigFactory
 
 object Main extends App {
   
   implicit val system = ActorSystem("AkkaKafkaTest")
   implicit val materializer = ActorMaterializer()
 
+  val config = ConfigFactory.load()
+
+  val servers = config.getString("test.kafka.servers")
+
   val producerSettings = ProducerSettings(system, new ByteArraySerializer, new StringSerializer)
-  .withBootstrapServers("localhost:9092")
+  .withBootstrapServers(servers)
 
   val done = Source(1 to 10)
     .map(_.toString)
     .map { elem =>
       println(s"Sink producer element: $elem")
-      new ProducerRecord[Array[Byte], String]("akka_test1", elem)
+      new ProducerRecord[Array[Byte], String]("akka-test1", elem)
     }
     .runWith(Producer.plainSink(producerSettings))
 
   val consumerSettings = ConsumerSettings(system, new ByteArrayDeserializer, new StringDeserializer)
-    .withBootstrapServers("localhost:9092")
+    .withBootstrapServers(servers)
     .withGroupId("akka-stream-kafka-test")
     .withProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest")
 
-  Consumer.committableSource(consumerSettings, Subscriptions.topics("akka_test1"))
+  Consumer.committableSource(consumerSettings, Subscriptions.topics("akka-test1"))
     .map(msg => println(msg))
     .runWith(Sink.ignore)
 
